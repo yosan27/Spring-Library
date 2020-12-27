7c70c1c45cefda7dd1248efa4386b50215addcf4
Mochamad Dwi Fadly <fadlydwi48@gmail.com>
2020-12-27 22:08:31 +0700

    update user entity, dto, controller, service

--- src/main/java/com/Faraday/Library/services/UserService.java
+++ src/main/java/com/Faraday/Library/services/UserService.java
@@ -1,5 +1,23 @@
 package com.Faraday.Library.services;
 
+import java.util.List;
+
+import com.Faraday.Library.dto.UserDto;
+import com.Faraday.Library.entity.UserEntity;
+
 public interface UserService {
+	
+	public List<UserEntity> getAll();
+	public List<UserEntity> getAllUserActiveById(Integer id);
+	public List<UserEntity> getAllUserActiveByUserCode(String userCode);
+	public List<UserEntity> getAllUserActiveByEmail(String email);
+	public UserEntity getAllUserByEmail(String email);
+	public UserEntity getAllUserByUserCode(String userCode);
+	public UserEntity getAllUserByStatus(Integer status);
+	public UserEntity insertUser(UserDto dto);
+	public UserEntity insertAdmin(UserDto dto);
+	public UserEntity updateUser(Integer id, UserDto dto);
+	public UserEntity updateUserPassword(Integer id, UserDto dto);
+	public UserEntity deleteUser(Integer id);
 
 }
