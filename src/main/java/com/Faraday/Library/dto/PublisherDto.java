5c0af9f1f5d352eb7288a1b595667b931a2c396e
erameilani <meilani.era@gmail.com>
2020-12-28 11:36:32 +0700

    set publisher code

--- src/main/java/com/Faraday/Library/dto/PublisherDto.java
+++ src/main/java/com/Faraday/Library/dto/PublisherDto.java
@@ -1,5 +1,52 @@
 package com.Faraday.Library.dto;
 
 public class PublisherDto {
+	private Integer id;
+	private String publisherCode;
+	private String publisherName;
+	private String address;
+	
+	public PublisherDto() {
+		super();
+	}
 
+	public PublisherDto(Integer id, String publisherCode, String publisherName, String address) {
+		super();
+		this.id = id;
+		this.publisherCode = publisherCode;
+		this.publisherName = publisherName;
+		this.address = address;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPublisherCode() {
+		return publisherCode;
+	}
+
+	public void setPublisherCode(String publisherCode) {
+		this.publisherCode = publisherCode;
+	}
+
+	public String getPublisherName() {
+		return publisherName;
+	}
+
+	public void setPublisherName(String publisherName) {
+		this.publisherName = publisherName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
--- src/main/java/com/Faraday/Library/repository/PublisherRepository.java
+++ src/main/java/com/Faraday/Library/repository/PublisherRepository.java
@@ -1,5 +1,11 @@
 package com.Faraday.Library.repository;
 
-public interface PublisherRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Faraday.Library.entity.PublisherEntity;
+
+@Repository
+public interface PublisherRepository extends JpaRepository<PublisherEntity, Integer> {
 
 }
