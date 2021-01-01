eb59997f68a6dd62ac3f5c8ddfb059b65e2a7704
Todi Dewantoro <todidewantoro@gmail.com>
2021-01-02 04:50:15 +0700

    menambahkan auto generate kode pada tabel category dan author, menambahkan response message post, mengganti length description menjadi 900

--- src/main/java/com/Faraday/Library/controllers/PublisherController.java
+++ src/main/java/com/Faraday/Library/controllers/PublisherController.java
@@ -2,18 +2,12 @@ package com.Faraday.Library.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.Faraday.Library.dto.PublisherDto;
 import com.Faraday.Library.services.PublisherServiceImplement;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api")
 public class PublisherController {
