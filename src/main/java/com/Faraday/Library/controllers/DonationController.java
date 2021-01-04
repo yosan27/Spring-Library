f418a0c52233e4e3eb15e1927bef1c693374b0c7
cleo <acesam222@gmail.com>
2021-01-05 00:27:32 +0700

    delete donation data

--- src/main/java/com/Faraday/Library/controllers/DonationController.java
+++ src/main/java/com/Faraday/Library/controllers/DonationController.java
@@ -2,6 +2,8 @@ package com.Faraday.Library.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.Faraday.Library.dto.DonationDto;
+import com.Faraday.Library.entity.DonationEntity;
 import com.Faraday.Library.services.DonationServiceImpl;
+import com.Faraday.Library.dto.StatusMessageDto;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class DonationController {
 	@Autowired
 	private DonationServiceImpl donation;
+	
+	@SuppressWarnings("rawtypes")
+	    private StatusMessageDto result = new StatusMessageDto();
+
 	
 	@GetMapping("/donation")
 	public ResponseEntity<?> getAll(){
@@ -42,5 +51,33 @@ public class DonationController {
 	@PutMapping("/donation/{id}")
 	public ResponseEntity<?> update(@PathVariable Integer id, @RequestBody DonationDto dto){
 		return ResponseEntity.ok(donation.updateStatus(id, dto));
+	}
+	
+//	softdelete donation data
+    @SuppressWarnings("unchecked")
+	@DeleteMapping("/donation/{id}")
+	public ResponseEntity<?> delete(@PathVariable Integer id){
+		try {
+			DonationEntity donationEntity = donation.delete(id);
+			
+			if( donationEntity == null) {
+				result.setStatus(400);
+                result.setMessage("Data not found");
+                result.setData(donationEntity);
+
+                return ResponseEntity.status(400).body(result);
+				
+			}
+			result.setStatus(200);
+            result.setMessage("deleted");
+            result.setData(donationEntity);
+
+            return ResponseEntity.status(200).body(result);
+			
+		}catch(Exception e) {
+			@SuppressWarnings("rawtypes")
+            StatusMessageDto error = new StatusMessageDto(500,e.getMessage(), null);
+            return ResponseEntity.status(500).body(error);
+		}
 	}
 }
