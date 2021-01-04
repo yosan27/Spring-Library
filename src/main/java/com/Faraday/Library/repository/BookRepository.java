067fb089423cb1c3c1af1ee1e7d9a86178e722f2
kodakon <riskifebiansyah@hotmail.com>
2021-01-04 19:48:38 +0700

    backlog catalog

--- src/main/java/com/Faraday/Library/repository/BookRepository.java
+++ src/main/java/com/Faraday/Library/repository/BookRepository.java
@@ -14,4 +14,7 @@ public interface BookRepository extends JpaRepository<BookEntity, Integer> {
 
     @Query(value = "SELECT * FROM book_entity WHERE status = 1", nativeQuery = true)
     List<BookEntity> findAllActive();
+    
+    @Query(value = "SELECT book_entity.id, book_entity.book_code, book_entity.isbn, book_entity.published_date, book_entity.status, book_entity.book_detail_code, book_detail_entity.book_title, book_detail_entity.cover, book_entity.author_code, author_entity.author_name,book_entity.category_code, category_entity.category_name, book_entity.publisher_code, publisher_entity.publisher_name FROM book_entity INNER JOIN book_detail_entity ON book_entity.book_detail_code = book_detail_entity.book_detail_code INNER JOIN author_entity ON book_entity.author_code = author_entity.author_code INNER JOIN category_entity ON book_entity.category_code = category_entity.category_code INNER JOIN publisher_entity ON book_entity.publisher_code = publisher_entity.publisher_code", nativeQuery = true)
+    List<BookEntity> findCatalog();
 }
