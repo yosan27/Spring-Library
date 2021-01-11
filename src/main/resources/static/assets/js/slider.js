44f811d55c68f631555396f82acbe065dc354db4
yosan27 <73471678+yosan27@users.noreply.github.com>
2021-01-11 12:52:22 +0700

    try deploy 3

--- src/main/resources/static/assets/js/slider.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $('#autoWidth,#autoWidth2').lightSlider({
-      autoWidth:true,
-      loop:true,
-      onSliderLoad: function() {
-          $('#autoWidth,#autoWidth2').removeClass('cS-hidden');
-      } 
-  });  
-});
